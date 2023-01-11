d2c1b94252ea85afe097dff850d40fbadc66af4e
KAPIL BENIWAL <120330634+KapilBeniwal1@users.noreply.github.com>
2023-01-11 18:20:33 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+class Product {
+  public int product(int x, int y) {
+    
+  }
+  
+  public int product(int x, int y, int z) {
+  
+  }
+  
+  public double product(double x, double y) {
+  
+  }
+ }
 
+public static void main(String args[]){
+   Product p = new Product();
+        p.product(2,5);
+        p.product(2,3,4);
+        p.product(20.02,50.20);
+}
 }
