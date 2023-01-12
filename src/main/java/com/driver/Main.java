9b6870f85a0fcd3b92aceb3a282e5585ae5cde3c
KAPIL  BENIWAL <kapilbeniwal2813@gmail.com>
2023-01-12 23:00:24 +0530

    Second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,24 +1,21 @@
 package com.driver;
 
 public class Main {
-class Product {
-  public int product(int x, int y) {
-    
-  }
-  
-  public int product(int x, int y, int z) {
-  
-  }
-  
-  public double product(double x, double y) {
-  
-  }
- }
-
-public static void main(String args[]){
-   Product p = new Product();
-        p.product(2,5);
-        p.product(2,3,4);
-        p.product(20.02,50.20);
-}
+    public static class  Product{
+        public int product(int x, int y){
+            return x*y;
+        }
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(15,25);
+        p.product(10,20,30);
+        p.product(5.5, 8.5);
+    }
 }
